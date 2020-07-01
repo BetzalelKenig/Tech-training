59fcc4f5fa2d9bf884111d598e8de814378cdc18
BetzalelKenig <59796236+BetzalelKenig@users.noreply.github.com>
2020-07-01 14:55:12 +0300

    cours card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { CoursCardComponent } from './cours-card/cours-card.component';
+import { LessonComponent } from './lesson/lesson.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursCardComponent,
+    LessonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
